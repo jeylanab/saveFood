c785bd50806d94bd36999ae73a343b33cd0c3814
jeylanab <jeylantesi@gmail.com>
2024-03-28 20:21:49 +0000

    added to do list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "./components/NavBar";
 import Hero from "./components/Hero";
+import Whatdo from "./components/Whatdo";
+
 
 const App = () => (
   <div>
     <Navbar className='  w-full overflow-hidden'/>
-    <Hero/>
+    <Hero />
+    <Whatdo/>
+
   </div>
 );
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,12 +7,13 @@ const Hero = () => {
           <div className='hero lg:flex flex  lg:justify-evenly lg:flex-row flex-col h-full items-center '>
               <div>
                   <div>
-                      <h1 className='font-poppins text-center font-bold lg:text-4xl py-5 sm:text-3xl'>Let's Dish Out Compassion: <br />SaveFood Heroes Unite!</h1>
+                      <h1 className='font-poppins text-center font-bold lg:text-4xl py-5 text-4xl '>Let's Dish Out Compassion: <br />SaveFood Heroes Unite!</h1>
                       <h3 className='lg:text-2xl text-md font-poppins text-center'> Serving Up Smiles, One Plate at a Time!</h3>
+                      <button className='btn  rounded-md text-white p-2 my-5 ml-10 font-poppins '> Explore</button>
                   </div>
               </div>
               <div>
-                  <img className='item-center' src={hero} alt="" />
+                  <img className='lg:py-0 py-10' src={hero} alt="" />
               </div>
               
           </div>
