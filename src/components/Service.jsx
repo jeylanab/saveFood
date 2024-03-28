99e5fb0e0b6ed69e36b964c2ffa8009c96d27f95
jeylanab <jeylantesi@gmail.com>
2024-03-28 21:12:01 +0000

    added card for service section

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -8,7 +8,9 @@ const Service = () => {
           <h1 className='primeColor btn rounded-md px-5 lg:text-6xl font-poppins font-bold text-3xl' >Services</h1>
           <p className='font-poppins px-5 lg:flex hidden'>programs and initiatives aimed at addressing hunger, promoting nutrition, <br />and reducing food waste within our communities.</p>
         </div>
-        
+        <div className="service-card">
+          
+        </div>
 
         
       </div>
